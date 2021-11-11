84eb9a4bee621891d993cdbb001945b8404434f6
Toshi <dtkellogg10@gmail.com>
2021-11-11 15:24:10 -0800

    Trying to get app to load faster.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/Nav";
 import Loading from "./components/Loading";
@@ -17,25 +17,22 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <ThemeProvider value={theme}>
-          <div className={theme}>
-            <div className="container">
-              <Nav toggleTheme={toggleTheme} />
-
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route exact path="/" render={() => <Posts type="top" />} />
-                  <Route path="/new" render={() => <Posts type="new" />} />
-                  <Route path="/post" component={Post} />
-                  <Route path="/user" component={User} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className="container">
+            <Nav toggleTheme={toggleTheme} />
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" render={() => <Posts type="top" />} />
+                <Route path="/new" render={() => <Posts type="new" />} />
+                <Route path="/post" component={Post} />
+                <Route path="/user" component={User} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
           </div>
-        </ThemeProvider>
-      </Router>
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
